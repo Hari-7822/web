987dd4e9b82ec451a5171327692fe8b576ac7a6a
hari-7822 <h26058549@gmail.com>
2022-04-09 00:53:19 +0530

    moorthi

---
+++ dist/js/html_quiz.js
@@ -0,0 +1,63 @@
+const data = [
+    {
+        Q : "What HTML stands for?",
+
+        a:"Hyper Text make language",
+        b:"Hyper Tension Maker Loop",
+        c:"Hyper Text Markup Language",
+        d:"Hover text main list",
+
+        crt : "c",
+        
+    },
+
+    {
+        Q : "Which tag is used to display headings",
+        
+        a : "<head> tag",
+        b : "<h1> to <h6> tag",
+        c : "<header> tag",
+        d : "none",
+
+        crt : "b",
+    },
+
+    {
+        Q : "when HTML is created?",
+
+        a : "1993",
+        b : "1999",
+        c : "1890",
+        d : "1970",
+        
+        crt : "a",
+    },
+
+    {
+        Q : "Who Developed HTML",
+        
+        a : "Bill Gates",
+        b : "Tim Berners Lee",
+        c : "Elon Musk",
+        d : "Charles Babbage",
+
+        crt : "b",
+    },
+
+    {
+        Q : "<meta http-equiv='X-UA-Compatible' content='IE=edge'/>\nWhy is this meta tag is used",
+
+        a : "to support Internet Explorer",
+        b : "to support Chrome",
+        c : "to support Firefox",
+        d : "none",
+
+        crt : "a",
+
+    },
+
+    {
+        
+    }
+
+]
