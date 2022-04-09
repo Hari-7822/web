8f6e4c2559398e3b4bedc9d3456f31c67c4797d1
hari-7822 <h26058549@gmail.com>
2022-04-09 07:19:21 +0530

    hari

--- dist/js/html_quiz.js
+++ dist/js/html_quiz.js
@@ -57,7 +57,116 @@ const data = [
     },
 
     {
-        
+        Q : "what is the use of <strong> tag.",
+
+        a : "to make the word strong",
+        b : "to change size of text",
+        c : "Change the color of the text",
+        d : "To make the corresponding text Bold",
+
+        crt : "d",
+    },
+
+    {
+
+        Q : "Why <select> tag is used.",
+
+        a : "to create lists",
+        b : "to create dropdown-list",
+        c : "to create blank spaces",
+        d : "to move texts",
+
+        crt : "b",    
+    },
+
+    {
+        Q : "where we usually save the title of the document",
+
+        a : "<body>",
+        b : "<script>",
+        c : "<head>",
+        d : "<footer>",
+
+        crt : "c",
+    },
+
+    {
+        Q : "",
+
+        a : "",
+        b : "",
+        c : "",
+        d : "",
+
+        crt : "",
+    },
+
+    {
+        Q : "",
+
+        a : "",
+        b : "",
+        c : "",
+        d : "",
+
+        crt : "",
+    },
+
+];
+
+const quiz = document.getElementsByClassName('quiz');
+
+const answer = document.querySelectorAll('.answer');
+const ques = document.getElementById('Q');
+
+const a = document.getElementById('a-text');
+const b = document.getElementById('b-text');
+const c = document.getElementById('c-text');
+const d = document.getElementById('d-text');
+
+const submit =  document.getElementById('finish');
+
+let current = 0;
+let score = 0;
+
+const deselectAns = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+};
+
+const sel = ()=> {
+    let answer;
+    answerElements.forEach((answerElement) => {
+        if(answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+};
+
+const loadQuiz =  () => {
+    deselectAns();
+
+    const currentQuiz = quizData[current];
+    questionElement.innerText = currentQuiz.question;
+
+    a.innerText = currentQuiz.a;
+    b.innerText = currentQuiz.b;
+    c.innerText = currentQuiz.c;
+    d.innerText = currentQuiz.d;
+};
+
+loadQuiz();
+
+submit.addEventListener("click", () => {
+    const answer = getSelected();
+
+    if(answer) {
+        if(answer === quizData[current].correct) score++;
+        currentQuiz++;
+    if(currentQuiz < quizData.length) loadQuiz();
+    else {
+        quiz.innerHTML  = <><h3>You scored ${score} of ${quizData.length} questions</h3><button onclick="history.go(0)">Play Again</button></>
+
+        if(score < 1) alert("Hii")
     }
 
-]
+    }
+});
